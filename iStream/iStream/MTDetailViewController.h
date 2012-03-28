ab762b7db1abb8a4e48a08f166593d52c898a7ef
tttthomasssss <th0mas.ko6er@gmail.com>
2012-03-28 18:36:42 +0200

    changed orgname flags in source files
    #lazyCheckIn

--- iStream/iStream/MTDetailViewController.h
+++ iStream/iStream/MTDetailViewController.h
@@ -3,7 +3,7 @@
 //  iStream
 //
 //  Created by Thomas Kober on 3/28/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Nimmermehr. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>

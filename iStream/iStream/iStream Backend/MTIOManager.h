4d52dd609e15b9240a5e263765406854807e50ae
tttthomasssss <th0mas.ko6er@gmail.com>
2012-04-19 23:01:07 +0100

    project file

---
+++ iStream/iStream/iStream Backend/MTIOManager.h
@@ -0,0 +1,13 @@
+//
+//  MTIOManager.h
+//  iStream
+//
+//  Created by Thomas Kober on 4/19/12.
+//  Copyright (c) 2012 Nimmermehr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MTIOManager : NSObject
+
+@end
---
+++ iStream/iStream/iStream Backend/MTIOManager.m
@@ -0,0 +1,13 @@
+//
+//  MTIOManager.m
+//  iStream
+//
+//  Created by Thomas Kober on 4/19/12.
+//  Copyright (c) 2012 Nimmermehr. All rights reserved.
+//
+
+#import "MTIOManager.h"
+
+@implementation MTIOManager
+
+@end

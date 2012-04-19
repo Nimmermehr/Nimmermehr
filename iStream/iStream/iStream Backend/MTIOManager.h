7e1ef2da4da2340be0cab645fc581119bc646d74
tttthomasssss <th0mas.ko6er@gmail.com>
2012-04-19 23:01:26 +0100

    started implementation of db model

--- iStream/iStream/iStream Backend/MTIOManager.h
+++ iStream/iStream/iStream Backend/MTIOManager.h
@@ -8,6 +8,26 @@
 
 #import <Foundation/Foundation.h>
 
-@interface MTIOManager : NSObject
+// Another singleton???
+
+@interface MTIOManager : NSObject {
+    BOOL _connected;
+    BOOL _cached;
+    NSUInteger chacheSize;
+    NSMutableDictionary *_cache;
+}
+
+@property (nonatomic)   BOOL        connected;
+@property (nonatomic)   BOOL        cached;
+@property (nonatomic)   NSUInteger  cacheSize;
+
+// Outcome depends whether or not the cache is on
+- (NSArray *)getNewsItems;
+- (NSArray *)getNewsItemsForServiceType:(NSString *)serviceType;
+- (NSArray *)getNewsItemsUpToIndex:(NSUInteger)index;
+- (NSArray *)getNewsItemsForServiceType:(NSString *)serviceType upToIndex:(NSUInteger)index;
+
+- (void)flushCache;
+- (void)reloadCache;
 
 @end
--- iStream/iStream/iStream Backend/MTIOManager.m
+++ iStream/iStream/iStream Backend/MTIOManager.m
@@ -7,7 +7,137 @@
 //
 
 #import "MTIOManager.h"
+#import "MTServiceConnectorManager.h"
+
+@interface MTIOManager (Private)
+- (void)initSharedInstance;
+@end
 
 @implementation MTIOManager
+
+@synthesize connected   = _connected;
+@synthesize cached      = _cached; // NSUserDefaults
+@synthesize cacheSize   = _cacheSize; // NSUserDefaults
+
+__strong static MTIOManager *_sharedInstance = nil;
+
++ (MTIOManager *)sharedIOManager
+{
+    if (_sharedInstance == nil) {
+        _sharedInstance = [[super allocWithZone:NULL] init];
+        
+        [_sharedInstance initSharedInstance];
+    }
+    
+    return _sharedInstance;
+}
+
+- (NSArray *)getNewsItems
+{
+    NSArray *allItems = [NSArray array];
+    
+    if (_cached) {
+        for (NSArray *item in [_cache allValues]) {
+            allItems = [allItems arrayByAddingObjectsFromArray:item];
+        }
+    } else {
+        // TODO: Load from DB
+    }
+    
+    return allItems;
+}
+
+- (NSArray *)getNewsItemsForServiceType:(NSString *)serviceType
+{
+    NSArray *items = [NSArray array];
+    
+    if (_cached) {
+        items = [_cache objectForKey:serviceType];
+    } else {
+        // TODO: Load From DB
+    }
+    
+    return items;
+}
+
+- (NSArray *)getNewsItemsUpToIndex:(NSUInteger)index
+{
+    return nil; // TODO:
+}
+
+- (NSArray *)getNewsItemsForServiceType:(NSString *)serviceType upToIndex:(NSUInteger)index
+{
+    return nil; // TODO:
+}
+
+- (void)flushCache
+{
+    [_cache removeAllObjects];
+}
+
+- (void)reloadCache
+{
+    
+}
+
+// TODO: needs some reworking
+- (void)setCacheSize:(NSUInteger)cacheSize
+{
+    // Kick out some items (only if actual cache is really larger) if cache is made smaller
+    if (cacheSize < [_cache count]) {
+        // TODO: shrink cache in that case (per item)
+    }
+    
+    _cacheSize = cacheSize;
+}
+
+#pragma Notification Handler
+
+- (void)handleTwitterNewsItemsReceived:(NSNotification *)notification
+{
+    
+}
+
+- (void)handleFacebookNewsItemsReceived:(NSNotification *)notification
+{
+    
+}
+
+- (void)handleGooglePlusNewsItemsReceived:(NSNotification *)notification
+{
+    
+}
+
+@end
+
+@implementation MTIOManager (Private)
+
+- (void)initSharedInstance
+{
+    _cache = [[NSMutableDictionary alloc] init];
+ 
+    // Register for all Notifications
+    MTServiceConnectorManager *mgr = [MTServiceConnectorManager sharedServiceConnectorManager];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(handleTwitterNewsItemsReceived:) 
+                                                 name:MTTwitterNewsItemsReceived 
+                                               object:mgr
+     ];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self 
+                                             selector:@selector(handleFacebookNewsItemsReceived:) 
+                                                 name:MTFacebookNewsItemsReceived 
+                                               object:mgr
+     ];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self 
+                                             selector:@selector(handleGooglePlusNewsItemsReceived:) 
+                                                 name:MTGooglePlusNewsItemsReceived 
+                                               object:mgr
+     ];
+    
+    // TODO: set up & connect db & fill cache
+}
 
 @end
